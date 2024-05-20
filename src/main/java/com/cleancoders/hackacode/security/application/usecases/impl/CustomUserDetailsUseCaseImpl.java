f558f99e58b8a5426d1b0a518c82fba6aceaadb5
JoelRondinelPacheco <joel.rondinel.pacheco@gmail.com>
2024-05-20 20:54:33 -0300

    Add custom user details

--- src/main/java/com/cleancoders/hackacode/security/application/usecases/impl/CustomUserDetailsUseCaseImpl.java
+++ src/main/java/com/cleancoders/hackacode/security/application/usecases/impl/CustomUserDetailsUseCaseImpl.java
@@ -1,13 +1,18 @@
 package com.cleancoders.hackacode.security.application.usecases.impl;
 
+import com.cleancoders.hackacode.common.UseCase;
 import com.cleancoders.hackacode.security.application.dto.user.UserDetailsDTO;
 import com.cleancoders.hackacode.security.application.port.out.UserAuthPort;
 import com.cleancoders.hackacode.security.application.usecases.CustomUsersDetailsUseCase;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 
 import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
 
+@UseCase
 public class CustomUserDetailsUseCaseImpl implements CustomUsersDetailsUseCase {
 
     @Autowired
@@ -15,9 +20,15 @@ public class CustomUserDetailsUseCaseImpl implements CustomUsersDetailsUseCase {
     @Override
     public UserDetailsDTO getUserDetails(String email) {
         UserDetailsDTO userDetails = this.userAuthPort.getUserDetails(email);
+        List<SimpleGrantedAuthority> authorities = userDetails.getRole().getPermissions().stream()
+                .map(each -> each.getOperation().getName())
+                .map(each -> new SimpleGrantedAuthority(each))
+                .collect(Collectors.toList());
+        authorities.add(new SimpleGrantedAuthority("ROLE_" + userDetails.getRole().getName()));
 
-        return null;
+        userDetails.setAuthorities(authorities);
 
+        return userDetails;
     }
 
     /*
